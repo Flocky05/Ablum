937160d276caa2c8c7d3f7afc093917408621c07
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-11 10:10:10 +0600

    addded popular section

---
+++ src/components/FoodCard/FoodCard.jsx
@@ -0,0 +1,20 @@
+
+import lock from "../../assets/images/card/lock.png"
+const FoodCard = (props) => {
+    const{name,img,price,body}=props.data;
+    return (
+        <div className="flex flex-col items-center border rounded-lg bg-gray-100 w-60 mx-auto m-4 shadow-lg">
+            <img className="w-40 h-40" src={img} alt="" />
+            <div className="text-center my-4">
+                <p className="text-lg font-semibold">{name}</p>
+                <p>{body}</p>
+                <div className="flex justify-between items-center">
+                    <h3 className="text-xl text-orange-600 font-semibold">{price}</h3>
+                    <img className="w-6 h-6 bg-gray-400 rounded-full" src={lock} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FoodCard;
--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from "react";
+import FoodCard from "../FoodCard/FoodCard.jsx";
 
 
 const Popular = () => {
+    const [foodcards,setFoodcard]=useState([]);
+
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res=>res.json())
+        .then(data=>setFoodcard(data))
+    },[])
     return (
         <div>
-            <h3 className="text-orange-500 italic text-2xl text-center p-4">The Best Food </h3>
+            <h3 className="text-orange-500 text-xl text-center p-4">The Best Food </h3>
             <h3 className="text-center text-3xl font-bold">Popular Dishes</h3>
-            
+                <div className="grid grid-cols-3 justify-items-center w-3/5 mx-auto">
+                {
+                   foodcards.map(foodcard=><FoodCard
+                   key={foodcard.id}
+                   data={foodcard}
+                   ></FoodCard>)
+                }
+                </div>
         </div>
     );
 };
