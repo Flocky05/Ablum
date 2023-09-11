9dbc478cdb19a7b24440048c3153e7e5700172ca
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-11 12:55:03 +0600

    update navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import img from "../../assets/images/body/logo.png"
 import moon from "../../assets/images/navbar/moon.png"
 const Navbar = () => {
     return (
-        <nav className="flex justify-between p-10 items-center text-xl font-semibold">
+        <nav className="flex justify-between p-10 items-center text-xl font-semibold bg-blue-950 text-white">
             <div className="flex items-center">
                 <img className="w-10 h-10" src={img} alt="" />
                 <a className="hover:text-orange-400" href="/">Ablum</a>
@@ -13,7 +13,7 @@ const Navbar = () => {
                 <a className="hover:text-orange-400" href="/about-us">About Us</a>
                 <a className="hover:text-orange-400" href="/popular">Popular</a>
                 <a className="hover:text-orange-400" href="/recently">Recently</a>
-                <img className="w-8 h-8" src={moon} alt="" />
+                <img className="w-8 h-8 invert" src={moon} alt="" />
 
             </div>
         </nav>
