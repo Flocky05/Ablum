4e775aa07576383ab8f3c4012b3c558b3ca1cadf
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-11 09:06:42 +0600

    added popular json data

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -3,7 +3,9 @@
 const Popular = () => {
     return (
         <div>
-            <h3>This is popular section</h3>
+            <h3 className="text-orange-500 italic text-2xl text-center p-4">The Best Food </h3>
+            <h3 className="text-center text-3xl font-bold">Popular Dishes</h3>
+            
         </div>
     );
 };
