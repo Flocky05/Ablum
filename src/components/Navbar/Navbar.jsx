cb394084068c970d8338485232d2db662282bd94
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-16 01:43:17 +0600

    update header section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,17 +3,17 @@ import img from "../../assets/images/body/logo.png"
 import moon from "../../assets/images/navbar/moon.png"
 const Navbar = () => {
     return (
-        <nav className="flex justify-between p-10 items-center text-xl font-semibold bg-blue-950 text-white">
+        <nav className="flex justify-around p-2 lg:p-10  text-md lg:text-xl lg:font-semibold bg-blue-950 text-white">
             <div className="flex items-center">
-                <img className="w-10 h-10" src={img} alt="" />
+                <img className=" w-4 h-4 lg:w-10 lg:h-10" src={img} alt="" />
                 <a className="hover:text-orange-400" href="/">Ablum</a>
             </div>
-            <div className="grid grid-cols-5 w-2/4">
+            <div className="grid grid-cols-5 justify-items-end w-4/5 lg:w-2/4">
                 <a className="hover:text-orange-400" href="/">Home</a>
                 <a className="hover:text-orange-400" href="/about-us">About Us</a>
                 <a className="hover:text-orange-400" href="/popular">Popular</a>
                 <a className="hover:text-orange-400" href="/recently">Recently</a>
-                <img className="w-8 h-8 invert" src={moon} alt="" />
+                <img className="w-4 h-4 lg:w-8 lg:h-8 invert" src={moon} alt="" />
 
             </div>
         </nav>
