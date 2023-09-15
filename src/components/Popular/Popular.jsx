a9f58365e0c2cff55f40a85407bc14a60804453d
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-16 01:29:57 +0600

    update popular section

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -14,7 +14,7 @@ const Popular = () => {
         <div>
             <h3 className="text-orange-500 text-xl text-center p-4">The Best Food </h3>
             <h3 className="text-center text-3xl font-bold">Popular Dishes</h3>
-                <div className="grid grid-cols-3 justify-items-center w-3/5 mx-auto">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center w-3/5 mx-auto">
                 {
                    foodcards.map(foodcard=><FoodCard
                    key={foodcard.id}
